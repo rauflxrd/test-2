c068f2b4e0b42eb88ce4b6a7af4748e9fcc07e5f
[rauf] <[raufam@code.edu.az]>
2022-12-21 21:24:33 +0400

    init

---
+++ test2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("new test");
+        }
+    }
+}
